dae58cacfe0ae5673d49d999b73f43442613c5c9
keatondj <keatondj@iastate.edu>
2018-11-22 21:42:17 -0600

    Initial

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import * as serviceWorker from "./serviceWorker";
+import Welcome from "./components/welcome";
+
+function tickTock(){
+  const element = <Welcome />;
+
+  ReactDOM.render(
+    element,
+    document.getElementById("clock"));
+
+}
+
+
+setInterval(tickTock, 1000);
+
+serviceWorker.register();
