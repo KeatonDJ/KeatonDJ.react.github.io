5897cf8798abf31cbd7b4bc328a32c7057f36ec0
keatondj <keatondj@iastate.edu>
2018-11-22 21:45:45 -0600

    Updates

---
+++ precache-manifest.db9a1e8ba38ae50b5f52b1933919fff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6135b0228394d2c205c4",
+    "url": "/KeatonDJReact/static/js/runtime~main.6135b022.js"
+  },
+  {
+    "revision": "a9a78aa137dac1c25250",
+    "url": "/KeatonDJReact/static/js/main.a9a78aa1.chunk.js"
+  },
+  {
+    "revision": "c2a028c79670602fb3e9",
+    "url": "/KeatonDJReact/static/js/1.c2a028c7.chunk.js"
+  },
+  {
+    "revision": "a9a78aa137dac1c25250",
+    "url": "/KeatonDJReact/static/css/main.c5bb1994.chunk.css"
+  },
+  {
+    "revision": "c2a028c79670602fb3e9",
+    "url": "/KeatonDJReact/static/css/1.5ad1189b.chunk.css"
+  },
+  {
+    "revision": "518850bd45cbb24f194b056281b30be6",
+    "url": "/KeatonDJReact/index.html"
+  }
+];
